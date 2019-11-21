4af2750ff9a4c4af6deac6eaaf2820044d3ffa55
AngelAmadeus <pabloangelma@hotmail.com>
2019-11-20 19:44:23 -0600

    advances on scala intro

--- activities/classwork/scala-intro/src/main/scala/example/c/model/Box.scala
+++ activities/classwork/scala-intro/src/main/scala/example/c/model/Box.scala
@@ -2,10 +2,12 @@ package example.c.model
 
 case class Box[A](value: A) {
 
+  def show(): Unit = println(value)
+
   def map[B](f: A => B): Box[B] = Box(f(value))
 
-  def show(fn: A => A): Box[A] = Box(fn(value))
+  def flatMap(f: A => Box[A]): Box[A] = f(value)
 
-  def flatMap[B](fn: A => Box[B]): Box[B] = fn(value)
+  def concat(other: Box[A]): Box[String] = Box(value.toString + other.value.toString)}
 
 }
